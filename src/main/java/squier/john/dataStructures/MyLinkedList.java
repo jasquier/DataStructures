2afd341facdf79f03dde65c20e90e0364c6270a5
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-02-27 07:38:17 -0500

    class and test framed out

---
+++ src/main/java/squier/john/dataStructures/MyLinkedList.java
@@ -0,0 +1,12 @@
+package squier.john.dataStructures;
+
+/**
+ * @author John A. Squier
+ */
+public class MyLinkedList<T> {
+
+
+    private class Node<T> {
+
+    }
+}
---
+++ src/test/java/squier/john/dataStructures/MyLinkedListTest.java
@@ -0,0 +1,16 @@
+package squier.john.dataStructures;
+
+import org.junit.Before;
+
+/**
+ * @author John A. Squier
+ */
+public class MyLinkedListTest {
+
+    MyLinkedList<Integer> myLinkedList;
+
+    @Before
+    public void setup() {
+        myLinkedList = new MyLinkedList<>();
+    }
+}
